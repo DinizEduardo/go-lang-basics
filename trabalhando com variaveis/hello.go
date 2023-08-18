27b9c48d318de17d0ee6d430cf76c60728629259
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-17 23:39:53 -0300

    Exibindo o menu

--- trabalhando com variaveis/hello.go
+++ trabalhando com variaveis/hello.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
 	nome := "Eduardo"
-	idade := 23
 	versao := 1.1
-	fmt.Println("Olá, sr.", nome, " sua idade é", idade)
+	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Esse programa está na versão", versao)
 
-	fmt.Println("O tipo da variavel nome é: ", reflect.TypeOf((versao)))
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O endeco da minha variavel comando é:", &comando)
+	fmt.Println("O comando escolhido foi:", comando)
 }
