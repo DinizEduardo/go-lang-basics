1d153a909a56521f714dde1c083ce297147b868b
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-17 23:33:58 -0300

    inferencia de tipos

--- trabalhando com variaveis/hello.go
+++ trabalhando com variaveis/hello.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var nome string = "Eduardo"
-	var idade int = 23
-	var versao float32 = 1.1
+	nome := "Eduardo"
+	idade := 23
+	versao := 1.1
 	fmt.Println("Olá, sr.", nome, " sua idade é", idade)
 	fmt.Println("Esse programa está na versão", versao)
+
+	fmt.Println("O tipo da variavel nome é: ", reflect.TypeOf((versao)))
 }
