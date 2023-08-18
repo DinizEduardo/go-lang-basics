26d28f2bce69cbe4959504afc1176e48ef705467
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-17 23:47:43 -0300

    Controle de fluxo com switch

--- 3 - controlando o fluxo do script/hello.go
+++ 3 - controlando o fluxo do script/hello.go
@@ -18,13 +18,14 @@ func main() {
 	fmt.Scan(&comando)
 	fmt.Println("O comando escolhido foi:", comando)
 
-	if comando == 1 {
+	switch comando {
+	case 1:
 		fmt.Println("Monitorando...")
-	} else if comando == 2 {
+	case 2:
 		fmt.Println("Exibindo logs...")
-	} else if comando == 0 {
+	case 0:
 		fmt.Println("Saindo...")
-	} else {
+	default:
 		fmt.Println("Não conheço este comando")
 	}
 }
