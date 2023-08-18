417a5d75b39698199ec27afe9fe80e6bcf8423c2
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-18 00:01:30 -0300

    Iniciando o monitoramento

---
+++ 4 - Fazendo requisicoes para a web/hello.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	exibeIntroducao()
+
+	exibeMenu()
+
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		iniciarMonitoramento()
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+}
+
+func exibeIntroducao() {
+	nome := "Eduardo"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Esse programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi:", comandoLido)
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br"
+	resp, err := http.Get(site)
+
+}
