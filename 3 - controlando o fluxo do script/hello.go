5decd528a2d2a79f7b52a9d9c94798b5d944f65c
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-17 23:57:01 -0300

    Introducao as funcoes

--- 3 - controlando o fluxo do script/hello.go
+++ 3 - controlando o fluxo do script/hello.go
@@ -2,21 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	nome := "Eduardo"
-	versao := 1.1
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Esse programa está na versão", versao)
+	exibeIntroducao()
 
-	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibir logs")
-	fmt.Println("0 - Sair do programa")
+	exibeMenu()
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi:", comando)
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -25,7 +19,30 @@ func main() {
 		fmt.Println("Exibindo logs...")
 	case 0:
 		fmt.Println("Saindo...")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 	}
+}
+
+func exibeIntroducao() {
+	nome := "Eduardo"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Esse programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi:", comandoLido)
+
+	return comandoLido
 }
