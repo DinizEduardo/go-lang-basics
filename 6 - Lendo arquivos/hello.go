48c8951b28a85c50261d120ff8d81b195be68cd7
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-22 00:06:46 -0300

    Arquivos em go

---
+++ 6 - Lendo arquivos/hello.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+const monitoramentos = 3
+const delay = 5
+
+func main() {
+	exibeIntroducao()
+	for {
+
+		exibeMenu()
+
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+	}
+}
+
+func exibeIntroducao() {
+	nome := "Eduardo"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Esse programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi:", comandoLido)
+	fmt.Println("")
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	//sites := []string{
+	//	"https://www.alura.com.br",
+	//	"https://caelum.com.br",
+	//	"https://google.com.br",
+	//	"https://youtube.com.br"}
+
+	sites := lerSitesDoArquivo()
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+
+	fmt.Println("")
+}
+
+func testaSite(site string) {
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("O site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("O site:", site, "está com problemas! Status code:", resp.StatusCode)
+	}
+}
+
+func lerSitesDoArquivo() []string {
+	var sites []string
+	arquivo, _ := os.Open("sites.txt")
+
+	fmt.Println(arquivo)
+
+	return sites
+}
